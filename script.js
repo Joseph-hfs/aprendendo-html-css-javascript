536baf4b5d8970e81140786bfbe5cb44371805a7
Joseph Hudson <111515355+Joseph-hfs@users.noreply.github.com>
2024-01-18 17:32:00 -0300

    pokedex

---
+++ script.js
@@ -0,0 +1,43 @@
+
+let quantidade = document.getElementById('quantidade');
+quantidade.addEventListener('keyup', () => {
+    pegaPokemons(quantidade.value);
+})
+44
+pegaPokemons(6);
+function pegaPokemons(quantidade) {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=10' + quantidade)
+        .then(response => response.json())
+        .then(allpokemon => {
+
+            var pokemons = [];
+
+            allpokemon.results.map((val) => {
+
+                fetch(val.url)
+                    .then(response => response.json())
+                    .then(pokemonSingle => {
+                        pokemons.push({ nome: val.name, imagem: pokemonSingle.sprites.front_default });
+
+                        if (pokemons.length == quantidade) {
+                            //finalizamos nossas requisições
+
+                            var pokemonBoxes = document.querySelector('.pokemon-boxes');
+                            pokemonBoxes.innerHTML = "";
+
+
+                            //console.log(pokemons);
+                            pokemons.map(function (val) {
+                                pokemonBoxes.innerHTML += `
+                            <div class="pokemon-box">
+                            <img src="`+ val.imagem + `">
+                                <h4>`+ val.nome + `</h4>
+                            </div>
+                            `
+                            })
+                        }
+                    })
+            })
+        })
+
+}
